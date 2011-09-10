efe69604b6e5cdda1607c6ca819101f9368d3d62
latrani <matt+github@latrani.net>
2011-09-10 02:05:41 -0700

    Ability to read jshint config info from user preferences

--- sublimelinter/modules/javascript.py
+++ sublimelinter/modules/javascript.py
@@ -35,17 +35,17 @@ def is_enabled():
         return (False, unicode(ex))
 
 
-def check(codeString, filename):
+def check(codeString, filename, jshint_config):
     path = jshint_path()
 
     if os.path.exists(jsc_path):
-        process = subprocess.Popen((jsc_path, os.path.join(path, 'jshint_jsc.js'), '--', str(codeString.count('\n')), '{}', path + os.path.sep),
+        process = subprocess.Popen((jsc_path, os.path.join(path, 'jshint_jsc.js'), '--', str(codeString.count('\n')), jshint_config, path + os.path.sep),
                                     stdin=subprocess.PIPE,
                                     stdout=subprocess.PIPE,
                                     stderr=subprocess.STDOUT,
                                     startupinfo=get_startupinfo())
     else:
-        process = subprocess.Popen(('node', os.path.join(path, 'jshint_node.js')),
+        process = subprocess.Popen(('node', os.path.join(path, 'jshint_node.js'), jshint_config),
                                     stdin=subprocess.PIPE,
                                     stdout=subprocess.PIPE,
                                     stderr=subprocess.STDOUT,
@@ -69,8 +69,10 @@ def check(codeString, filename):
 
 def run(code, view, filename='untitled'):
 
+    jshint_config = json.dumps(view.settings().get("jshint_config") or {})
+
     try:
-        errors = check(code, filename)
+        errors = check(code, filename, jshint_config)
     except OSError as (errno, message):
         print 'SublimeLinter: error executing linter: {0}'.format(message)
         errors = []
