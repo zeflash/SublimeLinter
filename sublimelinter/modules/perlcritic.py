9308955176401fdd01e244cc4d40b22390c7aa72
Nathan Levin-Greenhaw <nathan@njlg.info>
2012-03-08 09:15:43 -0800

    cleaning up file and adding Windows support with perlcritic.bat

--- sublimelinter/modules/perlcritic.py
+++ sublimelinter/modules/perlcritic.py
@@ -1,44 +1,39 @@
 # -*- coding: utf-8 -*-
-# perlcritic.py - sublimelint package for checking perl files
+# perlcritic.py - sublimelint package for critiquing perl files
 
 import re
-#import subprocess
+import os
+import subprocess
 
 from base_linter import BaseLinter
 
 CONFIG = {
     'language': 'perlcritic',
     'executable': 'perlcritic',
-    'test_existence_args': '--version',
-    'lint_args': '{filename}',
-    'input_method': 3,
+    'lint_args': ['--verbose', '8'],
 }
 
 
 class Linter(BaseLinter):
-    def parse_errors_perl(self, view, errors, lines, errorUnderlines, violationUnderlines, warningUnderlines, errorMessages, violationMessages, warningMessages):
-        for line in errors.splitlines():
-            match = re.match(r'(?P<error>.+?) at .+? line (?P<line>\d+)(, near "(?P<near>.+?)")?', line)
 
-            if match:
-                error, line = match.group('error'), match.group('line')
-                lineno = int(line)
-                near = match.group('near')
+    def get_executable(self, view):
+        """ check for perlcritic or perlcritic.bat (on windows machine) """
+        exe = 'perlcritic'
 
-                if near:
-                    error = '{0}, near "{1}"'.format(error, near)
-                    self.underline_regex(view, lineno, '(?P<underline>{0})'.format(re.escape(near)), lines, errorUnderlines)
+        if os.name == 'nt':
+            exe = 'perlcritic.bat'
 
-                self.add_message(lineno, lines, error, errorMessages)
+        try:
+            subprocess.call([exe, '--version'], startupinfo=self.get_startupinfo())
+            return (True, exe, 'using ' + exe)
+        except OSError:
+            return (False, '', 'Perl::Critic is required')
 
-    def underline_word(self, view, lineno, position, underlines):
-        # Assume lineno is one-based, ST2 wants zero-based line numbers
-        lineno -= 1
-        line = view.full_line(view.text_point(lineno, 0))
-        position += line.begin()
-
-        word = view.word(position)
-        underlines.append(word)
+    def executable_check(self, view, code, filename):
+        # make sure get_executable() has run and everything is setup
+        self.check_enabled(view)
+        # return the normal executable_check()
+        return super(Linter, self).executable_check(view, code, filename)
 
     def parse_errors(self, view, errors, lines, errorUnderlines, violationUnderlines, warningUnderlines, errorMessages, violationMessages, warningMessages):
         for line in errors.splitlines():
