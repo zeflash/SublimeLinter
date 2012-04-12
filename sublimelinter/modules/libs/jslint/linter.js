69a4b5dae732ec90dc148ac77aa26a0c38415ca1
Jacob Swartwood <jswartwood@gmail.com>
2012-04-11 21:29:41 -0400

    Converting jslint support to leverage new simpler js-runtime api.

--- sublimelinter/modules/libs/jslint/linter.js
+++ sublimelinter/modules/libs/jslint/linter.js
@@ -1,17 +1,17 @@
-/*jshint node: true */
-/*globals LINTER_PATH load */
+/*jslint node: true, sloppy: true */
+/*globals LINTER_PATH, load */
 
-var JSHINT = require("./jshint").JSHINT;
+var JSLINT = require("./jslint").JSLINT;
 
 exports.lint = function (code, config) {
     var results = [];
 
     try {
-        JSHINT(code, config);
+        JSLINT(code, config);
     } catch (e) {
         results.push({line: 1, character: 1, reason: e.message});
     } finally {
-        JSHINT.errors.forEach(function (error) {
+        JSLINT.errors.forEach(function (error) {
             if (error) {
                 results.push(error);
             }
