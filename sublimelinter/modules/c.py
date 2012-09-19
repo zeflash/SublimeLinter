93a215913c4a5222239a8e6642e6ce12bb2acd7f
Petter Strandmark <petter.strandmark@gmail.com>
2012-09-19 23:26:25 +0200

    C module uses parser from C++

--- sublimelinter/modules/c.py
+++ sublimelinter/modules/c.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 # cpp.py - sublimelint package for checking C files
 
-import re
-
-from base_linter import BaseLinter
 import cpp
 
 CONFIG = {
@@ -14,25 +11,6 @@ CONFIG = {
 for prop in ['executable', 'lint_args', 'input_method']:
     CONFIG[prop] = cpp.CONFIG[prop]
 
-class Linter(BaseLinter):
-    CPPCHECK_RE = re.compile(r'\[.+?:(\d+?)\](.+)')
-
+class Linter(cpp.Linter):
     def __init__(self, config):
         super(Linter, self).__init__(config)
-
-    def parse_errors(self, view, errors, lines, errorUnderlines,
-                     violationUnderlines, warningUnderlines,
-                     errorMessages, violationMessages,
-                     warningMessages):
-        # Go through each line in the output of cppcheck
-        for line in errors.splitlines():
-            match = self.CPPCHECK_RE.match(line)
-            if match:
-                # The regular expression matches the line number and
-                # the message as its two groups.
-                lineno, message = match.group(1), match.group(2)
-                # Remove the colon at the beginning of the message
-                if len(message) > 0 and message[0] == ':':
-                    message = message[1:].strip()
-                lineno = int(lineno)
-                self.add_message(lineno, lines, message, errorMessages)
