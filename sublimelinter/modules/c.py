962433e3c4452ef64305672fe5152fc9bdd599de
Jacob Swartwood <jswartwood@gmail.com>
2012-09-19 23:37:17 -0400

    cppcheck now uses INPUT_METHOD_TEMP_FILE.

--- sublimelinter/modules/c.py
+++ sublimelinter/modules/c.py
@@ -3,13 +3,13 @@
 
 import re
 
-from base_linter import BaseLinter, INPUT_METHOD_FILE
+from base_linter import BaseLinter, INPUT_METHOD_TEMP_FILE
 
 CONFIG = {
     'language': 'C',
     'executable': 'cppcheck',
     'lint_args': ['--enable=style', '--quiet', '{filename}'],
-    'input_method': INPUT_METHOD_FILE
+    'input_method': INPUT_METHOD_TEMP_FILE
 }
 
 
